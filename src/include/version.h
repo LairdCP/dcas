6b18dfb38902a99b36a878519cdbd8117e1596a7
Service DevOps Buildserver <service.devops.buildserver@lairdtech.com>
2018-02-18 20:47:03 +0000

    bump to build number 3.5.5.53

--- src/include/version.h
+++ src/include/version.h
@@ -3,7 +3,7 @@
 	#error "error: undefined development version - please define via build system"
 #endif
 
-#define LRD_BUILD_NUMBER "3.5.5.52"
+#define LRD_BUILD_NUMBER "3.5.5.53"
 
 #ifndef LAIRD_SDK_MSB
 #error "error: API version defines not present"
