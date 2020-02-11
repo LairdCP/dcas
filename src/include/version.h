8cba50365ddea93e9794db4924d88a32c9cca4b2
Service DevOps SCM <service.devops.scm@lairdconnect.com>
2020-02-11 20:11:23 +0000

    bump to build number 7.0.0.289

--- src/include/version.h
+++ src/include/version.h
@@ -3,7 +3,7 @@
 	#error "error: undefined development version - please define via build system"
 #endif
 
-#define LRD_BUILD_NUMBER "7.0.0.287"
+#define LRD_BUILD_NUMBER "7.0.0.289"
 
 #ifndef LAIRD_SDK_MSB
 #error "error: API version defines not present"
