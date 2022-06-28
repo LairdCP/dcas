8bfc8a66a036c826a7b9075f066bce53cba7139b
Service Build Slave <it.devops@lairdconnect.com>
2022-06-28 12:36:30 +0000

    bump to build number 7.0.0.696

--- src/include/version.h
+++ src/include/version.h
@@ -3,7 +3,7 @@
 	#error "error: undefined development version - please define via build system"
 #endif
 
-#define LRD_BUILD_NUMBER "7.0.0.695"
+#define LRD_BUILD_NUMBER "7.0.0.696"
 
 #ifndef LAIRD_SDK_MSB
 #error "error: API version defines not present"
